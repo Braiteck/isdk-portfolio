643909e2285e81f2d05046bdb78f6101fd46bb46
Vitaliy Sulimenko <prevetal@gmail.com>
2022-07-19 19:00:06 +0300

    Обновления
    Заменено видео главное
    Добавлен open graph

--- send.php
+++ send.php
@@ -9,7 +9,7 @@
     $message .= "E-mail: " . $data->email . "<br>";
     $message .= "Текст сообщения: " . $data->text . "<br>";
 
-    $headers  = "Content-type: text/html; charset=windows-1251 \r\n";
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n";
     $headers .= "Reply-To: ". $data->email ."\r\n";
 
     mail($to, $subject, $message, $headers);
